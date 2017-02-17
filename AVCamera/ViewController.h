261b93b5380ad0c4191ba10cc9317670b24972d3
Thinh M. Le <thinh.m.le@gmail.com>
2017-02-17 15:36:59 -0800

    Initial Commit

---
+++ AVCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVCamera
+//
+//  Created by Thinh Le on 2017-02-17.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
