884ca3fd366d8ffe8436c0c01deb92f44bbda78c
Thinh M. Le <thinh.m.le@gmail.com>
2017-02-17 16:01:43 -0800

    Add first function, i.e. a snapshot camera

---
+++ AVCamera/AVCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVCameraViewController.h
+//  AVCamera
+//
+//  Created by Thinh Le on 2017-02-17.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVCameraViewController : UIViewController
+
+@end
---
+++ AVCamera/AVCameraViewController.m
@@ -0,0 +1,37 @@
+//
+//  AVCameraViewController.m
+//  AVCamera
+//
+//  Created by Thinh Le on 2017-02-17.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import "AVCameraViewController.h"
+
+@interface AVCameraViewController ()
+
+@end
+
+@implementation AVCameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
